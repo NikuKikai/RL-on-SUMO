fada723d0877a7e0cbf5b59582a086bc224f0f2d
Pavel <pavel.rastopchin@intel.com>
2019-09-12 16:52:28 +0300

    First commit

--- agent.py
+++ agent.py
@@ -1,5 +1,107 @@
 import tensorflow as tf
 import numpy as np
+from deep_q_network import DQN
+
+import math
+import random
+import numpy as np
+from collections import namedtuple
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+from replay_memory import Transition
+
+class fixed_target_Agent():
+    '''
+    this clas implements Fixed-q target agent with pytorch
+    '''
+    def __init__(self, state_size, n_actions, device='cpu'):
+        # Get number of actions from gym action space
+        self.device = device
+
+        # Exploration / Exploitation params.
+        self.steps_done = 0
+        self.eps_start = 0.9
+        self.eps_end = 0.05
+        self.eps_decay = 200
+
+        # Learning params
+        self.batch_size = 128
+        self.discount = 0.999
+
+        # Deep q networks
+        self.policy_net = DQN(state_size, n_actions).to(device)
+        self.target_net = DQN(state_size, n_actions).to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
+
+        self.optimizer = optim.RMSprop(self.policy_net.parameters())
+        self.memory = ReplayMemory(10000)
+
+    def select_action(self, state):
+        global steps_done
+        sample = random.random()
+        eps_threshold = self.exp_end + (self.exp_start - self.eps_end) * \
+                        math.exp(-1. * self.steps_done / self.eps_decay)
+        steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                # t.max(1) will return largest column value of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                return self.policy_net(state).max(1)[1].view(1, 1)
+        else:
+            return torch.tensor([[random.randrange(n_actions)]], device=self.device, dtype=torch.long)
+
+    def optimize_model(self):
+        if len(self.memory) < self.batch_size:
+            return
+        transitions = memory.sample(self.batch_size)
+        # This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                      batch.next_state)), device=self.device,
+                                      dtype=torch.uint8)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                           if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.batch_size, device=self.device)
+        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.discount) + reward_batch
+
+        # Compute Huber loss
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # clip gradients.
+        # TODO: explain why
+        for param in self.policy_net.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
+
 
 class Agent:
     def __init__(self):
---
+++ deep_q_network.py
@@ -0,0 +1,54 @@
+import torch
+
+# class ConvDQN(nn.Module):
+#     '''
+#     This class implements deep convolutional q network with pytorch
+#     '''
+#     def __init__(self, h, w, outputs):
+#         super(DQN, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+#         self.bn1 = nn.BatchNorm2d(16)
+#         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+#         self.bn2 = nn.BatchNorm2d(32)
+#         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+#         self.bn3 = nn.BatchNorm2d(32)
+#
+#         # Number of Linear input connections depends on output of conv2d layers
+#         # and therefore the input image size, so compute it.
+#         def conv2d_size_out(size, kernel_size = 5, stride = 2):
+#             return (size - (kernel_size - 1) - 1) // stride  + 1
+#         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+#         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+#         linear_input_size = convw * convh * 32
+#         self.head = nn.Linear(linear_input_size, outputs)
+#
+#     # Called with either one element to determine next action, or a batch
+#     # during optimization. Returns tensor([[left0exp,right0exp]...]).
+#     def forward(self, x):
+#         x = F.relu(self.bn1(self.conv1(x)))
+#         x = F.relu(self.bn2(self.conv2(x)))
+#         x = F.relu(self.bn3(self.conv3(x)))
+#         return self.head(x.view(x.size(0), -1))
+
+class DQN(nn.Module):
+    '''
+    This class implements deep convolutional q network with pytorch
+    '''
+    def __init__(self, inputs, outputs):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(inputs, 64)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(64, 32)
+        self.fc2 = nn.BatchNorm1d(32)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(32, 16)
+        self.fc3 = nn.BatchNorm1d(16)
+        self.relu3 = nn.ReLU()
+        self.head = nn.Linear(16, outputs)
+
+    def forward(self, x):
+        x = self.relu1(self.bn1(self.fc1(x)))
+        x = self.relu2(self.bn2(self.fc2(x)))
+        x = self.relu3(self.bn3(self.fc3(x)))
+        return self.head(x.view(x.size(0), -1))
