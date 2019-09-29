72e10c82ecac2e3272d9cd031b08f5d867b26d04
Pavelrst <rwcutout@gmail.com>
2019-09-29 11:10:50 -0700

    disable plt warnings

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
+from experiments import DoubleIsraelNumOfEpisodes
 
-import sumoenv as se
-from rl_args import fixed_q_targets_args
-from traffic_system_manager import TrafficSystemManager
+### disable some plt warnings.
+import warnings
+import matplotlib.cbook
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
+###
 
-args = fixed_q_targets_args()
+def main():
+    exp = DoubleIsraelNumOfEpisodes()
+    exp.train_default()
 
-env = se.SumoEnv(args, path_to_sim_file='simulations\\israel_double_intersection.sumocfg', gui_f=False)
-manager = TrafficSystemManager(env.get_road_structure(), args)
-
-for _ in range(args.episodes):
-    state = env.reset(heatup=args.sim_heatup)
-    done = False
-    while not done:
-        action = manager.select_action(state)
-        next_state, reward, done = env.do_step(action)
-        manager.add_to_memory(state, action, next_state, reward) # TODO: missing done
-        manager.teach_agents() # try to optimize if enough samples in memory.
-        state = next_state
-    env.close()
-    manager.plot_learn_curve()
+main()
