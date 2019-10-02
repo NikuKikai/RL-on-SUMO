9234263ec67de602448a6355b8f1cc218ca8a432
Pavelrst <rwcutout@gmail.com>
2019-10-02 14:42:30 -0700

    args inherit from defualt args

--- rl_args.py
+++ rl_args.py
@@ -1,120 +1,100 @@
+class default_args():
+    '''
+    In this class we define the default arguments.
+    '''
+    def __init__(self):
+        # Exploration
+        self.eps_start = 0.99
+        self.eps_end = 0.05
 
-class fixed_q_targets_args():
+        # RL param
+        self.discount = 0.999
+
+        # Simulation params
+        self.sim_heatup = 20  # simulation run this amount of steps after reset.
+        self.sim_max_steps = 100 # 3000 # simulation ends after this amount of steps.
+        self.sim_steps = 3  # simulation steps performed between two actions.
+        self.state_type = 'density_and_speed'  # 'density', 'position', 'density_and_speed'
+        self.reward_type = 'wt_sum_absolute'  # 'wt_sum_absolute', 'wt_avg_absolute', 'wt_sum_relative', 'wt_max', 'accumulated_wt_max', 'wt_squares_sum'
+
+        # Replay params
+        self.replay_size = 50000  # capacity of replay.
+
+        # Deep q networks params
+        self.batch_size = 512 # batch size feed into deep q net.
+        self.optimizer = 'rmsprop' # Adam works bad.
+
+
+class single_intersection_args(default_args):
+    def __init__(self):
+        super().__init__()
+        # Simulation params
+        self.sim_file = 'simulations\\israel_single_intersection.sumocfg'
+        # RL param
+        self.episodes = 5  # 200 should be enough
+        self.eps_decay = 100  # exploration decay rate. bigger is slower. Don't set it above 10000 - it too slow.
+
+class double_intersection_args(default_args):
+    def __init__(self):
+        super().__init__()
+        # Simulation params
+        self.sim_file = 'simulations\\israel_double_intersection.sumocfg'
+        # RL param
+        self.episodes = 5 # 300
+        self.eps_decay = 100  # exploration decay rate. bigger is slower. Don't set it above 10000 - it too slow.
+
+
+'''
+### ARGUMENTS FOR DIFFERENT AGENTS AND DIFFERENT ENVIRONMENTS ###
+'''
+
+class fixed_q_targets_single_intersection_args(single_intersection_args):
     '''
     After experimenting I (pavel) came up with those params.
     It should be compared with other agents.
     '''
-    # Exploration / Exploitation params.
-    eps_start = 0.99
-    eps_end = 0.05
-    eps_decay = 100  # exploration decay rate. bigger is slower. Don't set it above 10000 - it too slow.
+    def __init__(self):
+        super().__init__()
+        # RL params
+        self.target_update = 50 # target net updated each k steps
+        self.rl_algo = 'fixed_q_targets'
+        # DL args
+        self.grad_clip = 1
 
-    # RL params
-    episodes = 100 # 100 should be enough
-    target_update = 50 # target net updated each k steps
-    discount = 0.999 # dsicount of future reward.
 
-    # Replay params
-    replay_size = 5000 # capacity of replay.
-
-    # Deep q networks params
-    batch_size = 512 # batch size feed into deep q net.
-    optimizer = 'rmsprop' # Adam works bad.
-
-    # Simulation params
-    sim_heatup = 50 # simulation run this amount of steps after reset.
-    sim_max_steps = 1000 # simulation ends after this ammount of steps.
-    sim_steps = 10 # simulation steps performed between two actions.
-    state_type = 'density'
-
-    # Logging params
-    output_file_name = 'fixed_q_targets_output.txt'
-
-class fixed_q_targets_israel_double_args():
+class double_dqn_single_intersection_args(single_intersection_args):
     '''
-    After experimenting I (pavel) came up with those params.
-    It should be compared with other agents.
+    No optimal args found
     '''
-    # Exploration / Exploitation params.
-    eps_start = 0.99
-    eps_end = 0.05
-    eps_decay = 100  # exploration decay rate. bigger is slower. Don't set it above 10000 - it too slow.
+    def __init__(self):
+        super().__init__()
+        self.target_update = 0.001  # polyak averaging
+        self.rl_algo = 'double_dqn'
+        # DL args
+        self.grad_clip = 1
 
-    # RL params
-    episodes = 100 # 100 should be enough
-    target_update = 50 # target net updated each k steps
-    discount = 0.999 # dsicount of future reward.
 
-    # Replay params
-    replay_size = 5000 # capacity of replay.
-
-    # Deep q networks params
-    batch_size = 512 # batch size feed into deep q net.
-    optimizer = 'rmsprop' # Adam works bad.
-
-    # Simulation params
-    sim_heatup = 50 # simulation run this amount of steps after reset.
-    sim_max_steps = 3000 # simulation ends after this amount of steps.
-    sim_steps = 10 # simulation steps performed between two actions.
-    state_type = 'density' # or 'position'
-    reward_type = 'wt_squares_sum' # 'wt_sum_absolute', 'wt_sum_relative', 'wt_max', 'accumulated_wt_max', 'wt_squares_sum'
-
-    # Logging params
-    output_file_name = 'fixed_q_targets_israel_double_output.txt'
-
-class dqn_args():
+class fixed_q_targets_double_intersection_args(double_intersection_args):
     '''
-    No optimal performance found
+    No optimal args found.
     '''
-    # Exploration / Exploitation params.
-    eps_start = 0.99
-    eps_end = 0.05
-    eps_decay = 100  # exploration decay rate. bigger is slower. Don't set it above 10000 - it too slow.
-
-    # RL params
-    episodes = 400 # 100 should be enough
-    target_update = None # target net updated each k steps
-    discount = 0.999 # dsicount of future reward.
-
-    # Replay params
-    replay_size = 5000 # capacity of replay.
-
-    # Deep q networks params
-    batch_size = 512 # batch size feed into deep q net.
-    optimizer = 'rmsprop' # Adam works bad.
-
-    # Simulation params
-    teleport_punishment = 50 # when vehicle waits too much it teleports. It should be punished.
-    sim_heatup = 50 # simulation run this amount of steps after reset.
-    sim_max_steps = 1500 # simulation ends after this ammount of steps.
-    sim_steps = 10 # simulation steps performed between two actions.
-
-    # Logging params
-    output_file_name = 'dqn_output.txt'
+    def __init__(self):
+        super().__init__()
+        # RL params
+        self.target_update = 50 # target net updated each k steps
+        self.rl_algo = 'fixed_q_targets'
+        # DL args
+        self.grad_clip = 1
 
 
-class code_sanity_args():
+class double_dqn_double_intersection_args(double_intersection_args):
     '''
+    No optimal args found
     '''
-    # Exploration / Exploitation params.
-    eps_start = 0.99
-    eps_end = 0.05
-    eps_decay = 100  # exploration decay rate. bigger is slower. Don't set it above 10000 - it too slow.
-
-    # RL params
-    episodes = 5 # 100 should be enough
-    target_update = 5 # target net updated each k steps
-    discount = 0.8 # dsicount of future reward.
-
-    # Replay params
-    replay_size = 5000 # capacity of replay.
-
-    # Deep q networks params
-    batch_size = 2048 # batch size feed into deep q net.
-    optimizer = 'rmsprop' # Adam works bad.
-
-    # Simulation params
-    teleport_punishment = 10 # when vehicle waits too much it teleports. It should be punished.
-    sim_heatup = 10 # simulation run this amount of steps after reset.
-    sim_max_steps = 500 # simulation ends after this ammount of steps.
-    sim_steps = 5 # simulation steps performed between two actions.
+    def __init__(self):
+        super().__init__()
+        # RL params
+        self.target_update = 0.001  # polyak averaging
+        self.rl_algo = 'double_dqn'
+        # DL args
+        self.grad_clip = None
