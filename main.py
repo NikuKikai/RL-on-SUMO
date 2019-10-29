c9afa9fba620443ad58f36153cda62aa5557e60e
Pavelrst <rwcutout@gmail.com>
2019-10-29 22:43:08 +0200

    main experiment removed from main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Agents.rl_args import *
 from utils.process_args import process_arguments
 from Environment.sumoenv import SumoEnv
 from Environment.traffic_system_manager import TrafficSystemManager
+from Experiments.experiments import main_experiment
 from datetime import datetime
 import os
 
@@ -11,52 +12,6 @@ import warnings
 import matplotlib.cbook
 warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
 ###
-
-
-
-def main_experiment(arguments):
-    list_experiments = [
-        {'path': './logs/compare/density__wt_max/', 'stype': 'density', 'rtype': 'wt_max'},
-        {'path': './logs/compare/position__wt_max/', 'stype': 'position', 'rtype': 'wt_max'},
-        {'path': './logs/compare/mean_speed__wt_max/', 'stype': 'mean_speed', 'rtype': 'wt_max'},
-        {'path': './logs/compare/queue__wt_max/', 'stype': 'queue', 'rtype': 'wt_max'},
-        {'path': './logs/compare/vehicle_types_emergency__wt_max/', 'stype': 'vehicle_types_emergency',
-         'rtype': 'wt_max'},
-        {'path': './logs/compare/density_and_speed__wt_max/', 'stype': 'density_and_speed', 'rtype': 'wt_max'},
-        {'path': './logs/compare/density_speed_emergency__wt_max/', 'stype': 'density_speed_emergency',
-         'rtype': 'wt_max'},
-        {'path': './logs/compare/density_queue__wt_max/', 'stype': 'density_queue', 'rtype': 'wt_max'},
-        {'path': './logs/compare/density_queue_mean_speed__wt_max/', 'stype': 'density_queue_mean_speed',
-         'rtype': 'wt_max'},
-        {'path': './logs/compare/density__wt_total_acc_relative/', 'stype': 'density', 'rtype': 'wt_total_acc_relative'},
-        {'path': './logs/compare/position__wt_total_acc_relative/', 'stype': 'position',
-         'rtype': 'wt_total_acc_relative'},
-        {'path': './logs/compare/mean_speed__wt_total_acc_relative/', 'stype': 'mean_speed',
-         'rtype': 'wt_total_acc_relative'},
-        {'path': './logs/compare/queue__wt_total_acc_relative/', 'stype': 'queue', 'rtype': 'wt_total_acc_relative'},
-        {'path': './logs/compare/vehicle_types_emergency__wt_total_acc_relative/', 'stype': 'vehicle_types_emergency',
-         'rtype': 'wt_total_acc_relative'},
-        {'path': './logs/compare/density_and_speed__wt_total_acc_relative/', 'stype': 'density_and_speed',
-         'rtype': 'wt_total_acc_relative'},
-        {'path': './logs/compare/density_speed_emergency__wt_total_acc_relative/', 'stype': 'density_speed_emergency',
-         'rtype': 'wt_total_acc_relative'},
-        {'path': './logs/compare/density_queue__wt_total_acc_relative/', 'stype': 'density_queue',
-         'rtype': 'wt_total_acc_relative'},
-        {'path': './logs/compare/density_queue_mean_speed__wt_total_acc_relative/', 'stype': 'density_queue_mean_speed',
-         'rtype': 'wt_total_acc_relative'},
-        {'path': './logs/compare/vehicle_types_emergency__wt_vehicle_class/', 'stype': 'vehicle_types_emergency',
-         'rtype': 'wt_vehicle_class'},
-        {'path': './logs/compare/density_speed_emergency__wt_max/', 'stype': 'density_speed_emergency',
-         'rtype': 'wt_max'},
-    ]
-    for exp_dict in list_experiments:
-        arguments.experiment_res_path = exp_dict['path']
-        arguments.state_type = exp_dict['stype']
-        arguments.reward_type = exp_dict['rtype']
-        exp = Experiment(arguments)
-        exp.train_default()
-    return
-
 
 def main(arguments):
     # Create folder for experiments results
@@ -78,7 +33,6 @@ def main(arguments):
         for key in vars(arguments):
             file.write(str(key) + ': ' + str(vars(arguments)[key]) + '\n')
 
-
     manager = TrafficSystemManager(env.get_dimensions(), arguments)
     for epi in range(arguments.episodes):
         print("### Starting Episode: ", epi, ' ###')
@@ -91,7 +45,7 @@ def main(arguments):
             manager.teach_agents()  # try to optimize if enough samples in memory.
             state = next_state
 
-        manager.dump_data_on_episode_end(res_dir_path)
+        manager.dump_data_on_episode_end(res_dir_path, plot=True)
         env.close()
     return
 
@@ -99,6 +53,9 @@ def main(arguments):
 if __name__ == '__main__':
     mode, args = process_arguments()
     if mode == 'experiment':
+        # Execute pre-determined scenarios
         main_experiment(args)
     else:
+        # Execute basic usage
+        args = cyclic_agent_double_intersection_args()
         main(args)
