be39791406db32cfbcae80c9a7a4dff5339220b2
Pavelrst <rwcutout@gmail.com>
2019-10-29 22:42:33 +0200

    main experiment moved to Experimnets.py

--- Experiments/experiments.py
+++ Experiments/experiments.py
@@ -43,3 +43,47 @@ class Experiment():
 
             manager.dump_data_on_episode_end(self.res_dir_path)
             self.env.close()
+
+
+def main_experiment(arguments):
+    list_experiments = [
+        {'path': './logs/compare/density__wt_max/', 'stype': 'density', 'rtype': 'wt_max'},
+        {'path': './logs/compare/position__wt_max/', 'stype': 'position', 'rtype': 'wt_max'},
+        {'path': './logs/compare/mean_speed__wt_max/', 'stype': 'mean_speed', 'rtype': 'wt_max'},
+        {'path': './logs/compare/queue__wt_max/', 'stype': 'queue', 'rtype': 'wt_max'},
+        {'path': './logs/compare/vehicle_types_emergency__wt_max/', 'stype': 'vehicle_types_emergency',
+         'rtype': 'wt_max'},
+        {'path': './logs/compare/density_and_speed__wt_max/', 'stype': 'density_and_speed', 'rtype': 'wt_max'},
+        {'path': './logs/compare/density_speed_emergency__wt_max/', 'stype': 'density_speed_emergency',
+         'rtype': 'wt_max'},
+        {'path': './logs/compare/density_queue__wt_max/', 'stype': 'density_queue', 'rtype': 'wt_max'},
+        {'path': './logs/compare/density_queue_mean_speed__wt_max/', 'stype': 'density_queue_mean_speed',
+         'rtype': 'wt_max'},
+        {'path': './logs/compare/density__wt_total_acc_relative/', 'stype': 'density', 'rtype': 'wt_total_acc_relative'},
+        {'path': './logs/compare/position__wt_total_acc_relative/', 'stype': 'position',
+         'rtype': 'wt_total_acc_relative'},
+        {'path': './logs/compare/mean_speed__wt_total_acc_relative/', 'stype': 'mean_speed',
+         'rtype': 'wt_total_acc_relative'},
+        {'path': './logs/compare/queue__wt_total_acc_relative/', 'stype': 'queue', 'rtype': 'wt_total_acc_relative'},
+        {'path': './logs/compare/vehicle_types_emergency__wt_total_acc_relative/', 'stype': 'vehicle_types_emergency',
+         'rtype': 'wt_total_acc_relative'},
+        {'path': './logs/compare/density_and_speed__wt_total_acc_relative/', 'stype': 'density_and_speed',
+         'rtype': 'wt_total_acc_relative'},
+        {'path': './logs/compare/density_speed_emergency__wt_total_acc_relative/', 'stype': 'density_speed_emergency',
+         'rtype': 'wt_total_acc_relative'},
+        {'path': './logs/compare/density_queue__wt_total_acc_relative/', 'stype': 'density_queue',
+         'rtype': 'wt_total_acc_relative'},
+        {'path': './logs/compare/density_queue_mean_speed__wt_total_acc_relative/', 'stype': 'density_queue_mean_speed',
+         'rtype': 'wt_total_acc_relative'},
+        {'path': './logs/compare/vehicle_types_emergency__wt_vehicle_class/', 'stype': 'vehicle_types_emergency',
+         'rtype': 'wt_vehicle_class'},
+        {'path': './logs/compare/density_speed_emergency__wt_max/', 'stype': 'density_speed_emergency',
+         'rtype': 'wt_max'},
+    ]
+    for exp_dict in list_experiments:
+        arguments.experiment_res_path = exp_dict['path']
+        arguments.state_type = exp_dict['stype']
+        arguments.reward_type = exp_dict['rtype']
+        exp = Experiment(arguments)
+        exp.train_default()
+    return
